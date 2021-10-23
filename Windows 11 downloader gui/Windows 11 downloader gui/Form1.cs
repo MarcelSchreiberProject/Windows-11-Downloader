e27b0ba82b6789dcf7040889323ec0215345cfaa
MarcelSchreiberProject <84279090+MarcelSchreiberProject@users.noreply.github.com>
2021-10-23 11:02:10 +0200

    Add files via upload

---
+++ Windows 11 downloader gui/Windows 11 downloader gui/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Windows_11_downloader_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("C:/Users/marce/Downloads/windows downloader/data/windows downloader.exe");
+        }
+    }
+}
---
+++ windows downloader/windows downloader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace windows_downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "https://software-download.microsoft.com/pr/Win11_EnglishInternational_x64.iso?t=cad34527-d346-4b02-8773-d0f19dccbd7f&e=1635022915&h=554f168377044bdbf679bfabcad2e688";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            System.Diagnostics.Process.Start(psi);
+        }
+    }
+    }
+
